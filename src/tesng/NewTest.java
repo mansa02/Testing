0bbd3cd49159f63ad29ffa5787df54c8d50db2c1
mansa02 <mansamishra99@gmail.com>
2019-06-04 15:41:04 +0530

    first commit

---
+++ src/tesng/NewTest.java
@@ -0,0 +1,16 @@
+package tesng;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Test the data");
+  }
+  @Test
+  public void run()
+  {
+	  System.out.println("Run the data accurately");
+  }
+}
---
+++ src/tesng/test.java
@@ -0,0 +1,10 @@
+package tesng;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
